a6477e0b7f76e9d5b91949ffa972cf6f6f03c7c8
Tushar Goyal <tushar462024@gmail.com>
2023-02-27 03:11:47 +0530

    Integrating and testing Chakra UI lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Center, ChakraProvider } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Center bg="tomato" height="100vh" color="white">
+        <Text fontSize="xx-large">Hello World!</Text>
+      </Center>
+    </ChakraProvider>
   );
 };
 
